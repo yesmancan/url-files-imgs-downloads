d2a8be012b57ebb675cacbc5e6fbc2c85e27afa5
Can Akburu <19973149+yesmancan@users.noreply.github.com>
2018-09-24 00:51:36 +0300

    https downloader added
    https downloader added

--- download.js
+++ download.js
@@ -1,17 +1,23 @@
 (function () {
   const fs = require('fs');
   const url = require('url');
-  const http = require('http');
-
+  let http = require('http');
   const DOWNLOAD_DIR = './downloads/';
 
   const download_file_httpget = function (file_url) {
-    var options = {
-      host: url.parse(file_url).host,
-      port: 80,
-      path: url.parse(file_url).pathname
-    };
+    const parseUrl = url.parse(file_url);
+    let portForProtocol = 80;
 
+    if (parseUrl.protocol === 'https:') {
+      http = require('https');
+      portForProtocol = 443;
+    }
+
+    var options = {
+      host: parseUrl.host,
+      port: portForProtocol,
+      path: parseUrl.pathname
+    };
     var file_name = url.parse(file_url).pathname.split('/').pop();
     var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
 
@@ -31,7 +37,7 @@
 
   */
 
-  download_file_httpget('https://cdn.okul.com.tr/galleries/3/1/630x420/16-year-old-artist-dimitra-milan-1.jpg')
+  download_file_httpget('http://oldmag.net/wp-content/uploads/2014/12/dali_moma_.jpg')
 
 
   /*
@@ -39,10 +45,12 @@
   Multi Downloads
 
   */
-  // var data = [{
+
+  
+  // let data = [{
   //     "count": {
-  //       "live": 43,
-  //       "prematch": 1038
+  //       "live": 15,
+  //       "prematch": 850
   //     },
   //     "id": 5,
   //     "label": "football",
@@ -51,16 +59,46 @@
   //   },
   //   {
   //     "count": {
-  //       "live": 4,
-  //       "prematch": 80
+  //       "live": 0,
+  //       "prematch": 139
   //     },
-  //     "id": 23,
-  //     "label": "basketball",
-  //     "name": "Basketbol",
+  //     "id": 2,
+  //     "label": "ice-hockey",
+  //     "name": "Buz Hokeyi",
+  //     "parentId": 1
+  //   },
+  //   {
+  //     "count": {
+  //       "live": 3,
+  //       "prematch": 162
+  //     },
+  //     "id": 10,
+  //     "label": "tennis",
+  //     "name": "Tenis",
+  //     "parentId": 1
+  //   },
+  //   {
+  //     "count": {
+  //       "live": 0,
+  //       "prematch": 8
+  //     },
+  //     "id": 71,
+  //     "label": "volleyball",
+  //     "name": "Voleybol",
+  //     "parentId": 1
+  //   },
+  //   {
+  //     "count": {
+  //       "live": 0,
+  //       "prematch": 22
+  //     },
+  //     "id": 18,
+  //     "label": "handball",
+  //     "name": "Hentbol",
   //     "parentId": 1
   //   }
   // ];
-  // data = data.map(m => m.label = 'https://m-sports.lvbet.com/img/icons/sports/' + m.label + '.svg')
+  // data = data.map(m => m.label = 'https://sports.lvbet.com/img/icons/sports/' + m.label + '.svg')
   // for (let i = 0; i < data.length; i++) {
   //   download_file_httpget(data[i])
   // }
